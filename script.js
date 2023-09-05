92b92777ad205931d17be3911aebe1e88a9109e7
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-09-06 03:20:41 +0530

    Passing Arguments Works: Values vs. Reference

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 ///////////////////////////////////////
 // Default Parameters
 
@@ -25,3 +26,34 @@ createbooking('IGDXB119', 2, 300);
 createbooking('AIUK181', 3, 200);
 createbooking('UAE911', 5);
 createbooking('QA9811', undefined, 800);
+*/
+
+///////////////////////////////////////
+// How Passing Arguments Works: Values vs. Reference
+
+const flight = 'IG911';
+const faiz = {
+  name: 'faiz khan',
+  passport: 9876543210,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'IGDXB811';
+  passenger.name = 'Mr.' + passenger.name;
+
+  if (passenger.passport === 9876543210) {
+    alert('Checked in');
+  } else {
+    alert('Wrong Passenger');
+  }
+};
+
+// checkIn(flight, faiz);
+// console.log(flight);
+// console.log(faiz);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000000);
+};
+newPassport(faiz);
+checkIn(flight, faiz);
