212adc5e1de7e07d18b5bc054704af1e5824bb09
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-09-06 08:09:01 +0530

    Functions Accepting Callback Functions

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ createbooking('QA9811', undefined, 800);
 
 ///////////////////////////////////////
 // How Passing Arguments Works: Values vs. Reference
-
+/*
 const flight = 'IG911';
 const faiz = {
   name: 'faiz khan',
@@ -57,3 +57,42 @@ const newPassport = function (person) {
 };
 newPassport(faiz);
 checkIn(flight, faiz);
+*/
+
+///////////////////////////////////////
+// Functions Accepting Callback Functions
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+
+
+/*
+/////////////////////////////////////////
+
+const ktm = function (model) {
+  const [firstt, ...other] = model.split(' ');
+  return [firstt.toUpperCase(), ...other].join(' ');
+};
+
+const compModel = function (model, fns) {
+  console.log(`your bike model is: ${fns(ktm)}`);
+};
+compModel('ktmrc200', ktm);
+*/
