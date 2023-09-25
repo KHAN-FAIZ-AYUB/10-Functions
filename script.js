26f6feee82ab474bacb5e635d87fb9a4fc9e3803
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-09-25 17:57:15 +0530

    Closure eg:1

--- script.js
+++ script.js
@@ -296,7 +296,7 @@ console.log(notPrivate);
 
 ///////////////////////////////////////
 // Closures
-
+/*
 const secureBooking = function () {
   let passengerCount = 0;
 
@@ -313,3 +313,34 @@ booker();
 booker();
 
 console.dir(booker);
+*/
+
+///////////////////////////////////////
+// More Closure Examples
+
+let f;
+
+const g = function () {
+  const a = 23;
+
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+// Re-assigning f function
+
+h();
+f();
+console.dir(f);
